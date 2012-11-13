a026f7271ebb6e17596272307480715fcc8c4704
Eric Schulte <eric.schulte@gmx.com>
2012-11-12 20:37:35 -0700

    ensure file exists

--- cilMutate.ml
+++ cilMutate.ml
@@ -27,8 +27,13 @@ let () = begin
   (* 1. read and parse arguments *)
   let collect arg = args := !args @ [arg] in
   let _ = Arg.parse speclist collect usage in
+  if (List.length !args) < 1 then begin
+    Printf.printf "You need to specify a program.\n";
+    exit 1
+  end;
+  let file = (List.nth !args 0) in
   (* 2. load the program into CIL *)
   (* 3. modify at the CIL level *)
   (* 4. write the results to STDOUT *)
-  Printf.printf "ids=%b number=%b\n" !ids !number
+  Printf.printf "ids=%b number=%b file=%s\n" !ids !number file
 end
